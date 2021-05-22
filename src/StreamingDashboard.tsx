63009124df1a793f8671ed61a3f6c855b8b99bec
Frank Riccobono <F.Riccobono@gmail.com>
2021-05-22 11:48:14 -0400

    WIP - starting to make the youtube form work

--- src/StreamingDashboard.tsx
+++ src/StreamingDashboard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {ChangeEvent} from 'react';
 
 interface DraftStream {
     title?: string;
@@ -10,17 +10,48 @@ export function StreamingDashboard() {
     const [rows, setRows] = React.useState<DraftStream[]>([]);
 
 
-    function addRow(){
-        setRows([...rows, {}]);
+    function addRow() {
+        setRows([...rows, {
+            title:"",
+            startTime:"",
+            endTime:""
+        }]);
     }
+
+    function submit() {
+        console.log(rows);
+    }
+
+    function updateRow(evt: ChangeEvent<HTMLInputElement>) {
+        const updatedRows = rows.map((row, index) => {
+            if (index === Number(evt.target.getAttribute("data-index"))) {
+                return {
+                    ...row,
+                    [evt.target.name]: evt.target.value
+                };
+            } else {
+                return row;
+            }
+        });
+        setRows(updatedRows);
+    }
+
     return <form>
-        <div><button type={"button"} onClick={addRow}>Add Row</button></div>
+        <div>
+            <button type={"button"} onClick={addRow}>Add Row</button>
+        </div>
         <table>
-        {rows.map( row =>
-        <tr key={row.title}>
-            <td><input type={"text"} /></td>
-            <td><input type={"datetime-local"} /></td>
-            <td><input type={"datetime-local"} /></td>
-        </tr> ) }
-    </table></form>;
+            <tbody>
+            {rows.map((row, index) =>
+                <tr key={index}>
+                    <td><input name="title" type={"text"} value={rows[index].title} data-index={index} onChange={updateRow}/></td>
+                    <td><input name="startTime" type={"datetime-local"} value={rows[index].startTime} data-index={index} onChange={updateRow}/></td>
+                    <td><input name="endTime" type={"datetime-local"} value={rows[index].endTime} data-index={index} onChange={updateRow}/></td>
+                </tr>)}
+            </tbody>
+        </table>
+        <div>
+            <button type={"button"} onClick={submit}>Save</button>
+        </div>
+    </form>;
 }
