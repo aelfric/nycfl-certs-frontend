7ad1824cce863c1ed89dbf157ade3fb5740be60d
Frank Riccobono <F.Riccobono@gmail.com>
2026-09-15 22:19:32 -0400

    fix lint

--- src/tournament-api.mock.ts
+++ src/tournament-api.mock.ts
@@ -23,19 +23,22 @@ export const restHandlers = [
     });
   }),
 
-  http.post<{ id: string }>("/certs/tournaments/:id", async ({ params, request}) => {
-    if (Number(params.id) < 0) {
-      return new HttpResponse(null, { status: 404 });
-    }
-    return HttpResponse.json(await request.json());
-  }),
+  http.post<{ id: string }>(
+    "/certs/tournaments/:id",
+    async ({ params, request }) => {
+      if (Number(params.id) < 0) {
+        return new HttpResponse(null, { status: 404 });
+      }
+      return HttpResponse.json(await request.json());
+    },
+  ),
 
-  http.put<{ id: string }>("/certs/tournaments/:id", async ({ params, request}) => {
+  http.put<{ id: string }>("/certs/tournaments/:id", async ({ params }) => {
     if (Number(params.id) < 0) {
       return new HttpResponse(null, { status: 404 });
     }
     return HttpResponse.json({
-      circuit: "updated"
+      circuit: "updated",
     });
   }),
 ];
